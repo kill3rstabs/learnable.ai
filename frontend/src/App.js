d80010c16d8869f6ff48e3f0a1f8c2801b87de9a
Muhammad Haris <m.haris1557@email.com>
2025-07-26 12:36:29 +0500

    Update link text to include both Nashit and Haris

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React with Nashit
+          Learn React with Nashit and Haris
         </a>
       </header>
     </div>
